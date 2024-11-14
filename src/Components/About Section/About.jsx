329143da5572087a531c4b43319cbe6ceee51ba0
mohdshadadhossain <shadadhussain1@hotmail.com>
2024-11-14 13:40:53 +0000

    Updated

--- src/Components/About Section/About.jsx
+++ src/Components/About Section/About.jsx
@@ -1,33 +1,32 @@
 import React from 'react'
 import './about.css'
-import img from '../../Assets/isra-png.png'
-import {VscCloudDownload} from  'react-icons/vsc'
+import img from '../../Assets/Picture.jpg'
+import { VscCloudDownload } from 'react-icons/vsc'
 // import cvv from '../../Assets/FRONTEND DEVELOPER.pdf'
 
 const About = () => {
   return (
     <section id='about' className='about section container'>
       <div class="sectionTitle">
-      <span class="titleNumber">01 . </span>  
-      <h5 class="titleText">About Me <div class="underline"><span></span></div></h5>
+        <span class="titleNumber">01 . </span>
+        <h5 class="titleText">About Me <div class="underline"><span></span></div></h5>
       </div>
 
       <div className="sectionContent grid">
         <div className="textSection">
           <h4>
-          I'm a success-driven, determined Web developer. I have experience using HTML Sass Javascript and PHP to build accessible component-based web pages and systems. <br/>
-          Ability to deliver quality results while collaborating in rapidly changing work environments and team compositions.
-          <br/>
-          A passionate person willing to learn more technologies to become a better developer than i was yesterday. So determined and result orietend.
+            Creative and detail-oriented front-end developer skilled in HTML, CSS, JavaScript, and Python. I focus on building responsive, accessible web applications that blend functionality with clean design.
+            <br />
+            With a passion for solving problems and refining my skills, I'm dedicated to delivering impactful digital experiences and continuously growing in the ever-evolving world of web development.
           </h4>
 
           <div className="aboutBtn">
-            <a className='flex' href="Dimas UML.docx" download="Dimas UML.docx" >Download CV <VscCloudDownload/></a>
+            <a className='flex' href="Dimas UML.docx" download="Dimas UML.docx" >Download CV <VscCloudDownload /></a>
           </div>
         </div>
-         <div className="aboutImgDiv">
-          <img className='aboutImg' src={img} alt="Israel Image" />
-         </div>
+        <div className="aboutImgDiv">
+          <img className='aboutImg' src={img} alt="Mohammed Image" />
+        </div>
       </div>
     </section>
   )
--- src/Components/Home Section/Home.jsx
+++ src/Components/Home Section/Home.jsx
@@ -1,56 +1,54 @@
 import React from 'react'
 import './home.css'
-import {TbArrowBigRightLines} from 'react-icons/tb'
-import {TbChevronsDown} from 'react-icons/tb'
-import {AiFillGithub} from 'react-icons/ai'
-import {TbBrandTwitter} from 'react-icons/tb'
-import {AiFillYoutube} from 'react-icons/ai'
+import { TbArrowBigRightLines } from 'react-icons/tb'
+import { TbChevronsDown } from 'react-icons/tb'
+import { AiFillGithub } from 'react-icons/ai'
+import { TbBrandTwitter } from 'react-icons/tb'
+import { AiFillYoutube } from 'react-icons/ai'
 
 const Home = () => {
   return (
     <section id='home' className='home section'>
       <div className="leftIcons">
         <div className="socials grid">
-            <a href="https://github.com" target="_blank"><AiFillGithub className="icon" /></a>
-            <a href="https://twitter.com/Aba_graphics" target="_blank"><TbBrandTwitter className="icon"/></a>
-           <a href="https://www.youtube.com/c/IsraTech1" target="_blank"> <AiFillYoutube className="icon"/></a>
-           <div className="line"></div>
+          <a href="https://github.com/mohdshadadhossain" target="_blank"><AiFillGithub className="icon" /></a>
+          <div className="line"></div>
         </div>
       </div>
-      
+
       <div className="container homeContent">
         <span className="introText">
-          Hi my name is,
+          Hi! My Name is
         </span>
         <h1 className="title">
-          Israel Abaho
+          Mohammed Shadad Hossain
         </h1>
         <span className="subTitle">
-          I develope scalable web based applications and software.
+          I build responsive and user-friendly interfaces for web applications.
         </span>
         <p className="homeParagraph">
-        I code within required coding standards and styles to serve the exact need.
+          I write clean, efficient code that aligns with modern design standards to deliver intuitive user experiences.
         </p>
 
         <div className="lowerHomeSection">
           <button className='contactBtn '>
-            <a className='flex'  href="#contact">
-            Contact Me <TbArrowBigRightLines className='icon'/>
+            <a className='flex' href="#contact">
+              Contact Me <TbArrowBigRightLines className='icon' />
             </a>
           </button>
 
           <div class="scrollDiv">
-              <a href="#about" className='flex'> <h6 class="scroll">Scroll Down <TbChevronsDown className='icon'/></h6></a>   
-           </div>
+            <a href="#about" className='flex'> <h6 class="scroll">Scroll Down <TbChevronsDown className='icon' /></h6></a>
+          </div>
         </div>
       </div>
 
       <div className="rightEmail">
         <div>
           <div className='emailAddress'>
-            <a href="https://mailto:isratech8@gmail.com" target="_blank">isratech8@gmail.com</a>
+            <a href="https://mailto:shadadhussain1@hotmail.com" target="_blank">shadadhussain1@hotmail.com</a>
           </div>
-           <div className="line"></div>
+          <div className="line"></div>
         </div>
       </div>
 
