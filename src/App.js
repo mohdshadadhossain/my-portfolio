d3fca522c74286d7ed313fb94b504dc677109620
mohdshadadhossain <shadadhussain1@hotmail.com>
2024-11-14 13:02:32 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './app.css'
+import About from './Components/About Section/About'
+import Contact from './Components/Contact Section/Contact'
+import Footer from './Components/Footer/Footer'
+import Home from './Components/Home Section/Home'
+import Navbar from './Components/Navbar/Navbar'
+import Project from './Components/Projects Section/Project'
+import Skills from './Components/Skills Section/Skills'
+
+
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+    <Home/>
+    <About/>
+    <Skills/>
+    <Project/>
+    <Contact/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
